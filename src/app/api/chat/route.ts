e6f38eb4a78c9e0a5f2f15d9bcc6af9219bb7c7b
clark <clarkwagdy1@gmail.com>
2025-08-13 23:57:38 +0300

    done

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,26 +4,26 @@ import axios from "axios";
 export async function POST(req: Request) {
   try {
  const { Context, input } = await req.json();
- const apiKey = process.env.HF_TOKEN;
+ const apiKey = process.env.HF_TOKEN?.replace("_",'');
  console.log( apiKey);
-   const response= await axios.post(
-      "https://router.huggingface.co/v1/chat/completions",
-   {
-        messages: [
-          {
-            role: "user",
-            content: `You must answer the following question using ONLY the information found in the provided context. and Simplify the answer with same language of question to egyptian students. If the answer is not explicitly stated in the context, respond exactly with:  Answer not found in context. Context: '${Context}' Question: '${input}'`,
-          },
-        ],
-        model: "openai/gpt-oss-120b:fireworks-ai",
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-          "Content-Type": "application/json",
-        },
-      }
-    )
+   const response = await axios.post(
+     "https://router.huggingface.co/v1/chat/completions",
+     {
+       messages: [
+         {
+           role: "user",
+           content: `You must answer the following question using ONLY the information found in the provided context. and Simplify the answer with same language of question to egyptian students. If the answer is not explicitly stated in the context, respond exactly with:  Answer not found in context. Context: '${Context}' Question: '${input}'`,
+         },
+       ],
+       model: "openai/gpt-oss-120b:fireworks-ai",
+     },
+     {
+       headers: {
+         Authorization: `Bearer hf_${apiKey}`,
+         "Content-Type": "application/json",
+       },
+     },
+   );
     // .then((response) => {
     //  return NextResponse.json(response.data, { status: 200 });
     // }).catch((error) => {
